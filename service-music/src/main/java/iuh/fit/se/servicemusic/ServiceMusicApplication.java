5102dadac19a0e455287ca4639e846d811848f69
DoMinhHung <dominhhung04032003@gmail.com>
2026-02-09 15:39:22 +0700

    add music service module with Spring Boot and PostgreSQL configuration

---
+++ service-music/src/main/java/iuh/fit/se/servicemusic/ServiceMusicApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.servicemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMusicApplication.class, args);
+    }
+
+}
